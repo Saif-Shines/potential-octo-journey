bf74f37ab7dcc4e039121a72568ab021c6a4e1c1
Saif Ali Shaik <saifshine7@gmail.com>
2021-04-11 10:34:55 +0530

    👌 IMPROVE: should return a user and repo

--- tests/services/gitService.spec.js
+++ tests/services/gitService.spec.js
@@ -1,10 +1,15 @@
+var chai = require('chai'),
+  sinon = require('sinon');
+chai.should();
 var gitService = require('../../services/gitService')();
 
 describe('Git Service', function () {
   describe('Get User', function () {
     it(`should return a user and repo`, async function () {
+      this.timeout(10000);
       let user = await gitService.getUser('Saif-Shines');
-      console.log(user);
+      user.login.should.equal('Saif-Shines');
+      user.should.have.property('repos');
     });
   });
 });
