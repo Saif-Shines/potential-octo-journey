7287ad213b6bf36f40df730874997a5c7b9ee184
Saif Ali Shaik <saifshine7@gmail.com>
2021-04-11 12:39:37 +0530

    👌 IMPROVE: getJson is template

--- tests/services/gitService.spec.js
+++ tests/services/gitService.spec.js
@@ -4,6 +4,52 @@ var chai = require('chai'),
 var PassThrough = require('stream').PassThrough;
 chai.should();
 var gitService = require('../../services/gitService')();
+var getJson = {
+  login: 'Saif-Shines',
+  id: 1,
+  node_id: 'MDQ6VXNlcjE=',
+  avatar_url: 'https://github.com/images/error/octocat_happy.gif',
+  gravatar_id: '',
+  url: 'https://api.github.com/users/octocat',
+  html_url: 'https://github.com/octocat',
+  followers_url: 'https://api.github.com/users/octocat/followers',
+  following_url: 'https://api.github.com/users/octocat/following{/other_user}',
+  gists_url: 'https://api.github.com/users/octocat/gists{/gist_id}',
+  starred_url: 'https://api.github.com/users/octocat/starred{/owner}{/repo}',
+  subscriptions_url: 'https://api.github.com/users/octocat/subscriptions',
+  organizations_url: 'https://api.github.com/users/octocat/orgs',
+  repos_url: 'https://api.github.com/users/octocat/repos',
+  events_url: 'https://api.github.com/users/octocat/events{/privacy}',
+  received_events_url: 'https://api.github.com/users/octocat/received_events',
+  type: 'User',
+  site_admin: false,
+  name: 'monalisa octocat',
+  company: 'GitHub',
+  blog: 'https://github.com/blog',
+  location: 'San Francisco',
+  email: 'octocat@github.com',
+  hireable: false,
+  bio: 'There once was...',
+  twitter_username: 'monatheoctocat',
+  public_repos: 2,
+  public_gists: 1,
+  followers: 20,
+  following: 0,
+  created_at: '2008-01-14T04:33:35Z',
+  updated_at: '2008-01-14T04:33:35Z',
+  private_gists: 81,
+  total_private_repos: 100,
+  owned_private_repos: 100,
+  disk_usage: 10000,
+  collaborators: 8,
+  two_factor_authentication: true,
+  plan: {
+    name: 'Medium',
+    space: 400,
+    private_repos: 20,
+    collaborators: 0
+  }
+};
 
 describe('Git Service', function () {
   describe('Get User', function () {
@@ -13,7 +59,6 @@ describe('Git Service', function () {
 
     it(`should return a user and repo`, async function () {
       this.timeout(10000);
-      var getJson = { login: 'Saif-Shines', repos: ['a'] };
       var gitResponse = new PassThrough();
       gitResponse.write(JSON.stringify(getJson));
       gitResponse.end();
