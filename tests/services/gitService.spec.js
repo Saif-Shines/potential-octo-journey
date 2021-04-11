104cf5310d922d9afe7a67453d59f7f3bba4e11b
Saif Ali Shaik <saifshine7@gmail.com>
2021-04-11 12:37:49 +0530

    👌 IMPROVE: http method is mocked

--- tests/services/gitService.spec.js
+++ tests/services/gitService.spec.js
@@ -1,15 +1,33 @@
 var chai = require('chai'),
-  sinon = require('sinon');
+  sinon = require('sinon'),
+  https = require('https');
+var PassThrough = require('stream').PassThrough;
 chai.should();
 var gitService = require('../../services/gitService')();
 
 describe('Git Service', function () {
   describe('Get User', function () {
+    beforeEach(function () {
+      this.request = sinon.stub(https, 'request');
+    });
+
     it(`should return a user and repo`, async function () {
       this.timeout(10000);
+      var getJson = { login: 'Saif-Shines', repos: ['a'] };
+      var gitResponse = new PassThrough();
+      gitResponse.write(JSON.stringify(getJson));
+      gitResponse.end();
+
+      this.request.callsArgWith(1, gitResponse).returns(new PassThrough());
+      // https.request = {};
       let user = await gitService.getUser('Saif-Shines');
+      console.log(user);
       user.login.should.equal('Saif-Shines');
       user.should.have.property('repos');
+    });
+
+    afterEach(function () {
+      this.request.restore();
     });
   });
 });
