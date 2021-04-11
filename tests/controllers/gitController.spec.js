b85e5b425a39aa17ba8dc778412aa51b33585f51
Saif Ali Shaik <saifshine7@gmail.com>
2021-04-11 13:18:13 +0530

    👌 IMPROVE: rewire somethign that is not available

--- tests/controllers/gitController.spec.js
+++ tests/controllers/gitController.spec.js
@@ -1,13 +1,25 @@
-var gitController = require('../../controllers/gitController')();
+var rewire = require('rewire');
+var GitCtrl = rewire('../../controllers/gitController');
+var gitController = GitCtrl();
 var chai = require('chai');
+var sinon = require('sinon');
 chai.should();
 
+var getUser;
 describe('gitController', function () {
+  beforeEach(function () {
+    var gitService = GitCtrl.__get__('gitService');
+    getUser = sinon.spy(gitService, 'getUser');
+    GitCtrl.__set__('gitService', gitService);
+  });
+
   it.only(`should get user and repos from git service`, function (done) {
+    this.timeout(5000);
     var req = { params: { userId: 'Saif-Shines' } };
     var res = { json: test };
 
     function test(user) {
+      console.log(getUser.getCall(0).args);
       user.login.should.equal('Saif-Shines');
       done();
     }
